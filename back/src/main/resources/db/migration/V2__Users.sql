fb92af9acfb40e58a1790dcd286f53d5c2004b1b
Danil1485 <danil.saykin.2016@gmail.com>
2025-06-29 14:41:10 +0500

    eslint fix for build

---
+++ back/src/main/resources/db/migration/V2__Users.sql
@@ -0,0 +1,7 @@
+INSERT INTO public.users(uuid, email, password, role)
+VALUES (
+        'ad25917a-3b86-49ae-b87f-359c28ac8699',
+        'admin@mail.ru',
+        '$2a$10$/jYW4ffvMPazxm.V9kcIDObntqDvkfMX5n2/f8keJ3JPSFfrlRoKa',
+        'ADMIN'
+       );
