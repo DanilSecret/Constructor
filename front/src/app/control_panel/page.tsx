b5cdb3f323744483ea654c98a28e63e17422d022
Shuni <kirillsemenyuta9@gmail.com>
2025-07-03 13:29:03 +0500

    update console_panel, users, user_create,users_edit

--- front/src/app/control_panel/page.tsx
+++ front/src/app/control_panel/page.tsx
@@ -35,25 +35,31 @@ export default function AdminPanel() {
     }
 
     return (
-        <div className="min-h-screen bg-[#F5F7FA] text-black">
-            <Header />
-            <div className="max-w-screen-md mx-auto px-4 py-20">
-                <h1 className="text-3xl font-bold text-center text-[#2C3E50] mb-10">
-                    Панель управления
-                </h1>
-                <div className="flex flex-col gap-6 items-center">
-                    <button
-                        onClick={() => router.push("/control_panel/full_history")}
-                        className="w-full sm:w-96 px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white text-lg rounded shadow transition"
-                    >
-                        Общая история
-                    </button>
-                    <button
-                        onClick={() => router.push("/control_panel/users")}
-                        className="w-full sm:w-96 px-6 py-3 bg-green-600 hover:bg-green-700 text-white text-lg rounded shadow transition"
-                    >
-                        Управление пользователями
-                    </button>
+        <div className="min-h-screen bg-[#F5F7FA] flex flex-col">
+            <Header/>
+            <div className="flex-1 flex justify-center items-center px-4 py-20">
+                <div
+                    className="w-full max-w-lg p-9 rounded-[10px] shadow-md bg-white border border-[#D5D8DC] flex flex-col">
+
+                    <h1 className="text-xl font-bold text-center text-[#2C3E50] mb-10">
+                        Панель управления
+                    </h1>
+
+                    <div className="flex flex-col gap-6 items-center">
+                        <button
+                            onClick={() => router.push("/control_panel/full_history")}
+                            className="w-full sm:w-96 px-6 py-3 bg-[#3498DB] hover:bg-[#2F89C5] text-white text-lg rounded shadow transition"
+                        >
+                            Общая история
+                        </button>
+                        <button
+                            onClick={() => router.push("/control_panel/users")}
+                            className="w-full sm:w-96 px-6 py-3 bg-[#3498DB] hover:bg-[#2F89C5] text-white text-lg rounded shadow transition"
+                        >
+                            Управление пользователями
+                        </button>
+                    </div>
+
                 </div>
             </div>
         </div>
